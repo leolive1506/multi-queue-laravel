3ff0ca66bba233745b0685e66b664f9e6f86fac6
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-03-08 16:23:39 -0300

    feat: especify connection

---
+++ app/Console/Commands/SendJobs.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\HighJob;
+use App\Jobs\LowJob;
+use Illuminate\Console\Command;
+
+class SendJobs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sj';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        HighJob::dispatch()->onConnection('database');
+        LowJob::dispatch();
+    }
+}
